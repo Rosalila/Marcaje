fccea1fdbcd201b7ec609b0862fd84076cd9f4de
Turupawn <turupawn@userr>
2014-10-14 10:25:34 -0600

    primer commit

---
+++ desktop/src/rosalila/studio/marcaje/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package rosalila.studio.marcaje.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import rosalila.studio.marcaje.MarcajeMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarcajeMain(), config);
+	}
+}
